65ed5e172c51598ab6dfb174888b3ada48040c7e
Jesse Lecy <jdlecy@gmail.com>
2023-09-04 03:52:27 -0700

    update data

--- data/prep-nccsdata-file.R
+++ data/prep-nccsdata-file.R
@@ -42,11 +42,37 @@ c(
 
 dd <- d[keep]
 
-dd$GEOTRACTID <- substr( d$GEOID10, 1, 11 )
+x <- dd$GEOID10
+dd$GEOID10 <- stringr::str_pad( x, 15, side="left", pad="0" )
+
+dd$GEOTRACTID <- substr( dd$GEOID10, 1, 11 )
+dd$GEOTRACTID <- paste0( "GEO-", dd$GEOTRACTID )
+
+dd$GEOID10 <- paste0( "GEO-", dd$GEOID10 )
+
+ntee <- read.csv( "ntee-crosswalk.csv" )
+
+ntee2 <- ntee[c("NTEE", "NTEE2")]
+
+dd <- merge( dd, ntee2, by.x="NTEEFINAL", by.y="NTEE", all.x=T )
+
+
+keep2 <- 
+c("EIN", "NAME", "SEC_NAME", 
+"RULEDATE", "SUBSECCD", "FNDNCD", "FRCD", 
+"NTEECC", "NTEEFINAL", "NTEE2", 
+"NAICS", "LEVEL1", "LEVEL2", "LEVEL3", "LEVEL4", 
+"FILER", "ZFILER", "OUTREAS", "OUTNCCS", 
+"TAXPER", "ACCPER", "INCOME", "ASSETS", 
+"CTAXPER", "CFINSRC", "CTOTREV", "CASSETS", 
+"ADDRESS", "CITY", "STATE", "ZIP5", 
+"Match_addr", "Longitude", "Latitude", 
+"Addr_type", "Score", "GEOID10", "GEOTRACTID")
+
+dd <- dd[keep2]
+
 
 write.csv( dd, "TEST-BMF-BLOCKS-JOINED.csv" )
-
-
 
 
 
